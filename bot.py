b64d4cab29ac3a0ea2b9858f186e2163fbd97a87
KillerBee88 <hardmen08@gmail.com>
2023-05-21 13:00:48 +0300

    Update bot.py
    publish photo to telegramm channel

--- bot.py
+++ bot.py
@@ -1,24 +1,23 @@
-from telegram import Bot, Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Bot
 import os
 from dotenv import load_dotenv
+from fetch_nasa_apod_images import fetch_nasa_apod_images
 
 load_dotenv()
 TG_API_KEY = os.getenv('TG_BOT_API')
+NASA_API_KEY = os.getenv('NASA_API_KEY')
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я твой новый бот.')
-
-def send_message_to_channel(bot, chat_id, message):
-    bot.send_message(chat_id=chat_id, text=message)
+def send_photo_to_channel(bot, chat_id, photo_url):
+    bot.send_photo(chat_id=chat_id, photo=photo_url)
 
 def main():
     bot = Bot(token=TG_API_KEY)
-    print(bot.get_me())
-    
     chat_id = "@test_channel2406"
-    message = "Это мое тестовое сообщение"
-    send_message_to_channel(bot, chat_id, message)
+
+    image_urls = fetch_nasa_apod_images(NASA_API_KEY, count=1)
+    
+    if image_urls:
+        send_photo_to_channel(bot, chat_id, image_urls[0])
 
 if __name__ == '__main__':
     main()
