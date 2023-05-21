7579b89ef5c5eb5853b08fa455633c0a9e4cccf2
KillerBee88 <hardmen08@gmail.com>
2023-05-21 12:43:19 +0300

    Create fetch_nasa_epic_images.py

---
+++ fetch_nasa_epic_images.py
@@ -0,0 +1,31 @@
+import argparse
+import requests
+import common
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv('NASA_API_KEY')
+
+def fetch_nasa_epic_images(api_key, days):
+    image_urls = []
+    base_url = "https://api.nasa.gov/EPIC/archive/natural/"
+
+    for i in range(days, 0, -1):
+        date = (datetime.now() - timedelta(days=i)).date()
+        date_str = date.strftime("%Y/%m/%d")
+        url = f"{base_url}{date_str}/png/epic_1b_{date.strftime('%Y%m%d')}0000059.png?api_key={api_key}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            image_urls.append(url)
+
+    return image_urls
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Fetch NASA EPIC images")
+    parser.add_argument('--days', type=int, default=10, help="Number of past days to fetch images for")
+    args = parser.parse_args()
+
+    nasa_epic_image_links = fetch_nasa_epic_images(API_KEY, args.days)
+    common.download_images(nasa_epic_image_links)
