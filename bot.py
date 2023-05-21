e282d569dd86b2917c86314f920364bc2514e91f
KillerBee88 <hardmen08@gmail.com>
2023-05-21 18:35:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,32 @@ from telegram import Bot
 import os
 from dotenv import load_dotenv
 from fetch_nasa_apod_images import fetch_nasa_apod_images
+from fetch_nasa_epic_images import fetch_nasa_epic_images
+from fetch_spacex_last_launch import fetch_spacex_last_launch
+import random
 
 load_dotenv()
 TG_API_KEY = os.getenv('TG_BOT_API')
 NASA_API_KEY = os.getenv('NASA_API_KEY')
 
+
 def send_photo_to_channel(bot, chat_id, photo_url):
     bot.send_photo(chat_id=chat_id, photo=photo_url)
+
 
 def main():
     bot = Bot(token=TG_API_KEY)
     chat_id = "@test_channel2406"
 
-    image_urls = fetch_nasa_apod_images(NASA_API_KEY, count=1)
+    fetch_functions = [fetch_nasa_apod_images, fetch_nasa_epic_images, fetch_spacex_last_launch]
+    fetch_func = random.choice(fetch_functions)
+
+    if fetch_func == fetch_spacex_last_launch:
+        image_urls = fetch_func()
+    else:
+        image_urls = fetch_func(NASA_API_KEY, count=1)
+
+    print(f"Fetched image URLs: {image_urls}")
     
     if image_urls:
         send_photo_to_channel(bot, chat_id, image_urls[0])
