91ec5a1d4bc24b28f1890379222daed5593a6087
KillerBee88 <hardmen08@gmail.com>
2023-05-23 22:19:01 +0300

    Update fetch_nasa_epic_images.py

--- fetch_nasa_epic_images.py
+++ fetch_nasa_epic_images.py
@@ -4,26 +4,30 @@ import common
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-API_KEY = os.getenv('NASA_API_KEY')
+from urllib.parse import urlencode, urljoin
 
 
 def fetch_nasa_epic_images(api_key, days):
     image_urls = []
     base_url = "https://api.nasa.gov/EPIC/archive/natural/"
 
-    for i in range(days, 0, -1):
-        date = (datetime.now() - timedelta(days=i)).date()
+    for day in range(days, 0, -1):
+        date = (datetime.now() - timedelta(days=day)).date()
         date_str = date.strftime("%Y/%m/%d")
-        url = f"{base_url}{date_str}/png/epic_1b_{date.strftime('%Y%m%d')}0000059.png?api_key={api_key}"
+        image_path = f"{date_str}/png/epic_1b_{date.strftime('%Y%m%d')}0000059.png"
+        params = {'api_key': api_key}
+        encoded_params = urlencode(params)
+        url = urljoin(base_url, f"{image_path}?{encoded_params}")
         response = requests.get(url)
-        if response.status_code == 200:
+        if response.ok:
             image_urls.append(url)
 
     return image_urls
 
 if __name__ == "__main__":
+    load_dotenv()
+    API_KEY = os.getenv('NASA_API_KEY')
+    
     parser = argparse.ArgumentParser(description="Fetch NASA EPIC images")
     parser.add_argument('--days', type=int, default=10, help="Number of past days to fetch images for")
     args = parser.parse_args()
