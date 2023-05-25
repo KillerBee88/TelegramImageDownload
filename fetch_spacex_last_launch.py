d0e1e0c423090395071e047c29d3544ccec81c26
KillerBee88 <hardmen08@gmail.com>
2023-05-25 21:39:12 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,18 +1,29 @@
-import argparse
 import common
 import requests
 
-def fetch_spacex_last_launch(launch_id=None):
+def fetch_spacex_last_launch_id():
+    response = requests.get('https://api.spacexdata.com/v5/launches')
+    response.raise_for_status()
+    spacex_launch_images = response.json()
+    last_launch_id = spacex_launch_images[-1].get('id')
+    return last_launch_id
+
+def fetch_spacex_last_launch(default_launch_id='5eb87d47ffd86e000604b38a'):
+    launch_id = fetch_spacex_last_launch_id()
     response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
     response.raise_for_status()
     launch_images_data = response.json()
     image_urls = launch_images_data.get('links', {}).get('flickr', {}).get('original', [])
+    
+    if not image_urls:
+        response = requests.get(f'https://api.spacexdata.com/v5/launches/{default_launch_id}')
+        response.raise_for_status()
+        launch_images_data = response.json()
+        image_urls = launch_images_data.get('links', {}).get('flickr', {}).get('original', [])
     return image_urls
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch SpaceX images")
-    parser.add_argument('--id', type=str, default='5eb87d47ffd86e000604b38a', help="ID of the launch")
-    args = parser.parse_args()
-
-    image_links = fetch_spacex_last_launch(args.id)
-    common.download_images(image_links)
+    image_links = fetch_spacex_last_launch()
+    
+    for url in image_links:
+        common.download_image(url)
