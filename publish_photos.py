20fb21920e789c1374b8d460da0bf51d19bd56f8
KillerBee88 <hardmen08@gmail.com>
2023-05-25 21:39:13 +0300

    Update publish_photos.py

--- publish_photos.py
+++ publish_photos.py
@@ -7,16 +7,14 @@ from telegram import Bot
 import argparse
 
 
-def send_photo(bot, photo_path):
-    load_dotenv()
-    CHANNEL_ID = os.getenv('CHANNEL_ID')
-    
+def send_photo(bot, chat_id, photo_path):
     with open(photo_path, 'rb') as photo:
-        bot.send_photo(chat_id=CHANNEL_ID, photo=photo)
+        bot.send_photo(chat_id, photo=photo)
 
 
 def publish_photo(bot, image_directory):
-    image_files = [f for f in os.listdir(image_directory) if os.path.isfile(os.path.join(image_directory, f))]
+    all_files = os.listdir(image_directory)
+    image_files = [f for f in all_files if os.path.isfile(os.path.join(image_directory, f))]
     random.shuffle(image_files)
     for image_file in image_files:
         send_photo(bot, os.path.join(image_directory, image_file))
@@ -24,18 +22,21 @@ def publish_photo(bot, image_directory):
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    
     parser = argparse.ArgumentParser(description='Публикация изображений в Телеграм канал каждые N часов.')
     parser.add_argument('dir', help='Директория с изображениями.')
     parser.add_argument('--hours', type=int, default=4, help='Интервал между постами в часах.')
     args = parser.parse_args()
-    
-    load_dotenv()
-    TG_API_KEY = os.getenv('TG_BOT_API')
-    
-    bot = Bot(token=TG_API_KEY)
-    schedule.every(args.hours).hours.do(publish_photo, bot=bot, image_directory=args.dir)
-    
+
+    token = os.getenv('TG_BOT_API')
+    chat_id = os.getenv('TG_CHAT_ID')
+
+    bot = Bot(token)
+    schedule.every(args.hours).hours.do(publish_photo, bot=bot, chat_id=chat_id, image_directory=args.dir)
+
     while True:
         schedule.run_pending()
         time.sleep(1)
+
 
