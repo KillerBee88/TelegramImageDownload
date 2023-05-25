db4d565b70097c983f79c8d71c3956567c89db36
KillerBee88 <hardmen08@gmail.com>
2023-05-25 21:39:07 +0300

    Update fetch_nasa_apod_images.py

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -8,17 +8,19 @@ import os
 def fetch_nasa_apod_images(api_key, count):
     response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={api_key}&count={count}')
     response.raise_for_status()
-    apod_images_data = response.json()
-    image_urls = [item['url'] for item in apod_images_data if 'image' in item['media_type']]
+    apod_images = response.json()
+    image_urls = [item['url'] for item in apod_images if 'image' in item['media_type']]
     return image_urls
 
 if __name__ == "__main__":
     load_dotenv()
-    API_KEY = os.getenv('NASA_API_KEY')
+    nasa_api_key = os.getenv('NASA_API_KEY')
     
     parser = argparse.ArgumentParser(description="Fetch NASA APOD images")
     parser.add_argument('--count', type=int, default=10, help="Number of images to fetch")
     args = parser.parse_args()
 
-    nasa_image_links = fetch_nasa_apod_images(API_KEY, args.count)
-    common.download_images(nasa_image_links)
+    nasa_image_links = fetch_nasa_apod_images(nasa_api_key, args.count)
+    
+    for url in nasa_image_links:
+        common.download_image(url)
