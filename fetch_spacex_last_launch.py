5286839608cb444453dda5542f90169ae8bd242a
KillerBee88 <hardmen08@gmail.com>
2023-05-26 21:45:39 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,25 +1,25 @@
 import common
 import requests
 
+def fetch_spacex_launch_images(launch_id):
+    response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
+    response.raise_for_status()
+    spacex_launch_images = response.json()
+    return spacex_launch_images.get('links', {}).get('flickr', {}).get('original', [])
+
 def fetch_spacex_last_launch_id():
     response = requests.get('https://api.spacexdata.com/v5/launches')
     response.raise_for_status()
     spacex_launch_images = response.json()
-    last_launch_id = spacex_launch_images[-1].get('id')
-    return last_launch_id
+    return spacex_launch_images[-1].get('id')
 
 def fetch_spacex_last_launch(default_launch_id='5eb87d47ffd86e000604b38a'):
     launch_id = fetch_spacex_last_launch_id()
-    response = requests.get(f'https://api.spacexdata.com/v5/launches/{launch_id}')
-    response.raise_for_status()
-    launch_images_data = response.json()
-    image_urls = launch_images_data.get('links', {}).get('flickr', {}).get('original', [])
+    image_urls = fetch_spacex_launch_images(launch_id)
     
     if not image_urls:
-        response = requests.get(f'https://api.spacexdata.com/v5/launches/{default_launch_id}')
-        response.raise_for_status()
-        launch_images_data = response.json()
-        image_urls = launch_images_data.get('links', {}).get('flickr', {}).get('original', [])
+        image_urls = fetch_spacex_launch_images(default_launch_id)
+    
     return image_urls
 
 if __name__ == "__main__":
